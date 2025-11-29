d2e67232a816e60c83649d94c86ca6322f68f5b6
DriaMT321 <dagneremt@gmail.com>
2025-11-29 14:06:59 -0400

    Migración de GraphQL a REST API para cursos/capacitaciones
    Cambios realizados:
    - api.js: Actualizado baseURL a http://192.168.0.4:8010/api
    - cursosService.js: Nuevo servicio REST para cursos (getCursosVoluntario, getAllCursos, asignarCurso)
    - queriesSQL.js: Migrado obtenerCursosPorVoluntarioId de GraphQL a REST
    - queriesNOSQL.js: Agregado manejo de errores (try/catch) para evitar crashes cuando GraphQL no disponible
    - mutationsNOSQL.js: Agregado manejo de errores graceful para emergencias/ubicación
    - CursosScreen.js: Usa cursosService en lugar de Apollo/GraphQL
    - EvaluacionesScreen.js: Removida dependencia de Apollo useQuery

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.13:8000/api',
+  baseURL: 'http://192.168.0.4:8010/api',
+  //baseURL: 'http://192.168.0.13:8000/api',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
